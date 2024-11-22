af47ba846e710c75f8ea797b3878c3401eb7ffd0
MobelRobinson <mobelm522@gmail.com>
2024-11-22 10:43:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function nxtpage() {
+    window.location.href = 'template-2.html';
+}
+function template() {
+    window.location.href = 'template.html';
+}
+function pgvisit() {
+    window.open('https://portfoliocraft-gjjm.github.io/portfolio-sample-1/')
+}
+function provisit() {
+    window.open('https://aruntechz.web.app/?fbclid=PAZXh0bgNhZW0CMTEAAabW3nBN0bfUKQ29TR61EAVuaGmr-sFXNLucFYNYgJ9dGJ88wjzCg69g9GA_aem_vQFCMxsnUsRVlPCbCaoJuA')
+}
+function pro() {
+    window.location.href='plan.html';
+}
+function dtls(){
+    window.location.href='gform.html';
+}
